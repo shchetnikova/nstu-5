1848a792ad002e6683d5bd5c8a1671f29d5d5520
Щетникова Катя <student@FB.LOCAL>
2022-12-06 10:29:32 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -19,3 +19,49 @@ function setEnglish(){
     document.getElementById('your-name').innerHTML = 'Your name';
     document.getElementById('input-name').placeholder = 'enter your name';
 }
+
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert('Привет, ' + userName + ' ' + userSurname + '\n' + 'Ваш возраст:' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+}
+
+function showHead () {
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' +  r + ',' + g + ',' + b + ')';
+    document.getElementById('student').style.backgroundColor=color;
+    alert ('Хэло');
+}
+
+function showFoot () {
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' +  r + ',' + g + ',' + b + ')';
+    document.getElementById('footer').style.backgroundColor=color;
+    alert ('До свидания!' );
+}
+
+function showFirst () {
+    document.getElementById('first').style.display='none';
+}
+function showSecond () {
+    document.getElementById('second').style.display='none';
+}
+function showThird () {
+    document.getElementById('third').style.display='none';
+}
+function showForth () {
+    document.getElementById('forth').style.display='none';
+}
+function showFifth () {
+    document.getElementById('fifth').style.display='none';
+}
