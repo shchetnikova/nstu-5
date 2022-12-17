fd15d622487d141409673e061f8d92e4e9c65a08
Щетникова Катя <sapronovaannaa@mail.ru>
2022-12-17 11:31:07 +0700

    Количество дней с даты рождения

---
+++ lab8.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', showDate);
+function showDate(params) {
+    let out = document.getElementById('today');
+    let today = new Date();
+    out.innerHTML = today.toLocaleDateString("ru-RU");
+}
+
+function showDaysCount (params) {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.floor(daysCount);
+    result.innerHTML = 'С даты рождения прошло дней: ' + daysCount;
+}
