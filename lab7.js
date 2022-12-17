f5fc4ce32f684b8a8d7698bba26a999cae29bc5a
Щетникова Катя <sapronovaannaa@mail.ru>
2022-12-17 09:25:23 +0700

    Лесенка

--- lab7.js
+++ lab7.js
@@ -65,3 +65,71 @@ function showAlert() {
     let color = 'rgb(' + r + ',' + g + ',' + b + ')';
     document.getElementById('user-data').style.backgroundColor = color;
 }
+
+function makeStairs(){
+    let stairs = document.querySelector('#stairs');
+    let stepCount = 20;
+    for(let i = 0; i<stepCount; i++) {
+        stairs.innerHTML += '<div class="step"></div>';
+    }
+
+    let steps = document.querySelectorAll('.step');
+    for (let i = 0; i<steps.length; i++){
+        steps[i].style.left = (30+10*i) + 'px';
+        steps[i].style.top = (300 + 80*i) + 'px';
+        steps[i].innerHTML = 'Блок номер' + i;
+    }
+}
+function makeSnezhki () {
+    let snezki = document.querySelector(' #snezki');
+    let stepCount = 40;
+    for (let i = 0; i<stepCount; i++){
+        snezki.innerHTML += '<img class="snezhinki" src="snezhinki.png">';
+    }
+    let snezhinkis = document.querySelectorAll('.snezhinki');
+    let screenWidth = window.screen.width;
+    let screenHeight = window.screen.height;
+
+    for (let i = 0; i<snezhinkis.length; i++) 
+    {
+        snezhinkis[i].style.left = Math.random() * (screenWidth-100) + 'px';
+        snezhinkis[i].style.top = Math.random() *(screenHeight-100) + 'px';
+        snezhinkis[i].style.width= Math.random() * 45 + 'px';
+    }
+}
+
+function Table() {
+    let stepCount = 9;
+
+    for (let i = 0; i<stepCount; i++){
+        document.write ((i+1) + '*27=' + ((i+1)*27) + "<br>");
+    }
+    for (let i = 0; i<stepCount; i++){
+        document.write ((i+1) + '*30=' + ((i+1)*30) + "<br>");
+    }
+}
+
+function Stikh() {
+    let steps = document.querySelectorAll('.Zima');
+    for (let i =0; i<steps.length; i++) {
+        
+        let r = Math.round(Math.random()*255);
+        let g = Math.round(Math.random()*255);
+        let b = Math.round(Math.random()*255);
+        steps[i].style.color = 'rgb(' +  r + ',' + g + ',' + b + ')';
+        r = Math.round(Math.random()*255);
+        g = Math.round(Math.random()*255);
+        b = Math.round(Math.random()*255);
+        steps[i].style.backgroundColor = 'rgb(' +  r + ',' + g + ',' + b + ')';
+        steps[i].style.fontSize = (i +10)  + 'px';
+        steps[i].style.width = (i*10 + 100) + 'px';
+
+    }
+}
+
+function Load() {
+    makeStairs () ;
+    makeSnezhki ();
+    Stikh();
+    
+}
